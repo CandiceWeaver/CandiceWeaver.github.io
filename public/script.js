97e9dc5213325730a0f6696786fa4ac04f5b002f
Candice Weaver <68248078+CandiceWeaver@users.noreply.github.com>
2021-07-01 16:19:02 +0100

    bug on mobile menu

--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
+'use strict';
 
+// Functionality for mobile menu
+const mobileMenu = document.querySelector('.navigation__mobile');
+const mobileMenuOpen = document.querySelector('.navigation__mobile--open');
+const mobileMenuClose = document.querySelector('.navigation__mobile--close');
+const navLinks = document.querySelector('.navigation__links');
+const navigation = document.querySelector('.navigation');
+
+mobileMenu.addEventListener('click', function () {
+  navLinks.classList.toggle('hidden');
+  mobileMenuOpen.classList.toggle('hidden');
+  mobileMenuClose.classList.toggle('hidden');
+  navigation.classList.toggle('active');
+});
