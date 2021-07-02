b6262f064520e13e0a64c66aaaea69e6995f7c48
Candice Weaver <68248078+CandiceWeaver@users.noreply.github.com>
2021-07-02 22:39:43 +0100

    updated layout

--- public/script.js
+++ public/script.js
@@ -5,11 +5,9 @@ const mobileMenu = document.querySelector('.navigation__mobile');
 const mobileMenuOpen = document.querySelector('.navigation__mobile--open');
 const mobileMenuClose = document.querySelector('.navigation__mobile--close');
 const navLinks = document.querySelector('.navigation__links');
-const navigation = document.querySelector('.navigation');
 
 mobileMenu.addEventListener('click', function () {
   navLinks.classList.toggle('hidden');
   mobileMenuOpen.classList.toggle('hidden');
   mobileMenuClose.classList.toggle('hidden');
-  navigation.classList.toggle('active');
 });
